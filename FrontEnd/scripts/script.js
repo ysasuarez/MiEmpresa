6e5a2f18c268e75253bf9157e1ec0a5774feb4df
ysasuarez <ysasuareez@gmail.com>
2023-10-19 19:08:59 +0200

    Lista de coches, detalles...

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,24 +1,57 @@
-// Realizar una solicitud GET a la API para obtener los datos de los coches
-fetch('http://localhost:8080/coches') // Ajusta la URL a tu punto de conexión en Spring Boot
-.then(response => response.json())
-.then(coches => {
-    const cochesTableBody = document.getElementById('cochesTableBody');
+document.addEventListener("DOMContentLoaded", () => {
+    const cochesTable = document.querySelector(".coches-table");
+    const cochesList = document.querySelector(".coches-list");
 
-    coches.forEach(coche => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${coche.id}</td>
-            <td>${coche.marca}</td>
-            <td>${coche.coste}</td>
-            <td>${coche.fechaIngreso}</td>
-            <td>${coche.vendido ? 'Sí' : 'No'}</td>
-            <td>${coche.matricula}</td>
-            <td>${coche.precioVenta}</td>
-        `;
+    function showDetails(coche, detailsButton, row) {
+        const detailsRow = cochesList.insertRow(row.rowIndex);
+        const detailsCell = detailsRow.insertCell(0);
+        detailsCell.colSpan = 4;
 
-        cochesTableBody.appendChild(row);
-    });
-})
-.catch(error => {
-    console.error('Error al obtener datos de coches:', error);
+        const detailsContainer = document.createElement('div');
+        detailsContainer.className = 'coche-details';
+
+        detailsContainer.innerHTML = `ID: ${coche.id}<br>
+                                Coste: ${coche.coste}<br>
+                                Fecha de Ingreso: ${coche.fechaIngreso}<br>
+                                Estado: ${coche.vendido ? 'Vendido' : 'Disponible'}`;
+
+        const closeButton = document.createElement('button');
+        closeButton.className = 'close-button';
+        closeButton.textContent = 'x';
+        closeButton.addEventListener('click', () => {
+            detailsRow.remove();
+            detailsButton.disabled = false;
+        });
+
+        detailsContainer.appendChild(closeButton);
+        detailsCell.appendChild(detailsContainer);
+
+        detailsButton.disabled = true;
+    }
+
+    fetch("http://localhost:8080/coches")
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Error al obtener los coches");
+            }
+        })
+        .then((data) => {
+            data.forEach((coche, index) => {
+                const row = cochesList.insertRow();
+                row.insertCell(0).textContent = coche.marca;
+                row.insertCell(1).textContent = coche.matricula;
+                row.insertCell(2).textContent = coche.precioVenta;
+                const actionsCell = row.insertCell(3);
+                const detailsButton = document.createElement('button');
+                detailsButton.className = 'button-details';
+                detailsButton.textContent = '+';
+                detailsButton.addEventListener('click', () => showDetails(coche, detailsButton, row));
+                actionsCell.appendChild(detailsButton);
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 });
